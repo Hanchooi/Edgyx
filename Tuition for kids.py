62cda8559274256c58e303949632ad9b97e759e3
Han <tsinchooi@gmail.com>
2021-04-08 08:30:32 +0800

    Create Tuition for kids.py

---
+++ Tuition for kids.py
@@ -0,0 +1,60 @@
+import random
+import time
+
+
+def game1():
+    counts = 3
+    answer = random.randint(1,10)
+    while counts > 0:
+        temp = input("Guess a number: ")
+        guess = int(temp)
+        
+        if guess == answer:
+            print("Wow, that's correct!")
+            print("No cookie for you though.\n")
+            break
+        else:
+            if guess < answer:
+                print("Too small!\n")
+                time.sleep(1)
+            else:
+                print("Too big!\n")
+                time.sleep(1)
+            counts = counts - 1
+        print("You have",counts,"chance left.")
+
+    if counts == 0:
+        print("Too Bad! \nGame Over")
+            
+def game2():
+    
+    counts = 1
+    answer = random.randint(1,10)
+    play = True
+    
+    while play:
+        temp = input("Guess a number: ")
+        guess = int(temp)
+        
+        if guess == answer:
+            print("Wow, that's correct! You got it right with",counts,"attempt(s)!")
+            print("No cookie for you though.\n")
+            break
+        else:
+            if guess < answer:
+                print("Too small!\n")
+                
+            else:
+                print("Too big!\n")
+        counts = counts + 1
+        reply = str(input("Play again? (Y/N): "))
+        
+        if reply == "N":
+            play = False
+            
+def game3():
+    wait = random.randint(1,10)
+    print("Ready...")
+    time.sleep(wait)
+    print("Go!")
+    
